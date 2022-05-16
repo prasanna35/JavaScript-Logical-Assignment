583210d4d712a429436bfb6227aabb0e36560efc
prasanna35 <91239101+prasanna35@users.noreply.github.com>
2022-05-16 11:40:13 +0530

    Add files via upload

---
+++ Assignment1-6.js
@@ -0,0 +1,10 @@
+function myFunction(){
+    var list1=["hello","world","in","a","frame"];
+    console.log("********");
+    for(i=0;i<listt.length;i++){
+        console.log("*"+list1[i]+"*");
+    }
+    console.log("********");
+
+}
+myFunction();
